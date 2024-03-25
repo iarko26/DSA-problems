7ae10a487d9889eab17a6bf598765cf94fce4719
Arnob-Ghosh <94540571+iarko26@users.noreply.github.com>
2024-03-26 02:25:08 +0600

    Update peak element.py

--- peak element.py
+++ peak element.py
@@ -22,3 +22,29 @@ def peak(arr,n):
 arr=[1,2,1,2,1]
 n=len(arr)
 print("Peak element is",peak(arr,n))
+
+
+
+
+# This is the geeksforgeeks version code its different from the previous in bases cases it checks for every single element
+class Solution:   
+    def peakElement(self,arr, n):
+        
+        n=len(arr)
+        if n==1:
+            return 0
+        if arr[0]>=arr[1]:
+            return 0
+        if arr[n-1]>=arr[n-2]:
+            return n-1
+        low=1
+        high=n-2
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid-1]<arr[mid] and arr[mid]>arr[mid+1]:
+                return mid
+            if arr[mid]<arr[mid+1]:
+                low=mid+1
+            else:
+                high=mid-1
+        return -1
